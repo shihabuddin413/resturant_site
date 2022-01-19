a9a5cb5e9ca60362df89bf00f42ef010a3165571
shihab <shihab38900@gmail.com>
2022-01-20 01:56:41 +0600

    tailwind website

---
+++ public/index.js
@@ -0,0 +1,10 @@
+var burger = document.querySelector("#burger");
+var menu = document.querySelector("#menu");
+
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "#c0f0c3",
+          200: "#8c8c5c",
+        },
+      },
+      fontFamily: {
+        body: ["Quicksand"],
+      },
+    },
+  },
+  plugins: [],
+};
